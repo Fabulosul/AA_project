27d2aeba5823554e23ea6d8a53ce9b851dd6cf8a
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2025-01-13 00:29:15 +0200

    changed welsh

--- src/algorithms/welsh_powell_graph_coloring.py
+++ src/algorithms/welsh_powell_graph_coloring.py
@@ -7,63 +7,25 @@ def has_edge(graph, u, v):
     '''Verifică dacă există o muchie între nodurile "u" și "v"'''
     return v in graph[u]
 
-def calculate_degrees(graph):
-    '''Calculează gradul fiecărui nod'''
-    return [(u, len(graph[u])) for u in range(len(graph))]
-
-def get_uncolored_highest_degree(degree_list, node_color):
-    '''Returnează nodul necolorat cu cel mai mare grad'''
-    for u, degree in degree_list:
-        if node_color[u] == -1:
-            return u
-    return None
-
-def update_V_prime(graph, current_colored_node, node_color):
-    '''Actualizează setul V' cu noduri necolorate care nu sunt vecine cu nodul curent colorat'''
-    V_prime = []
-    active_color = node_color[current_colored_node]
-    for v in range(len(graph)):
-        if (
-            node_color[v] == -1 and  # Nod necolorat
-            not has_edge(graph, current_colored_node, v) and  # Nu este adiacent nodului curent
-            all(node_color[neighbor] != active_color for neighbor in graph[v])  # Niciun vecin nu are culoarea curentă
-        ):
-            V_prime.append(v)
-    return V_prime
-
 def graph_coloring_welsh_powell(graph):
-    '''Colorează graful utilizând algoritmul Welsh-Powell'''
+    '''Colorează graful folosind algoritmul Welsh-Powell'''
     nr_nodes = len(graph)
-    node_color = [-1] * nr_nodes  # -1 indică noduri necolorate
-    color_set = []  # Lista de culori utilizate
+    node_color = [-1] * nr_nodes
 
-    # Step 1: Calculează gradele și sortează descrescător
-    degrees = calculate_degrees(graph)
-    degrees.sort(key=lambda x: x[1], reverse=True)
+    # Calculeaza gradele și sorteaza nodurile descrescător după grad
+    degrees = sorted(range(nr_nodes), key=lambda u: len(graph[u]), reverse=True)
 
-    # Step 4: Repetă până toate nodurile sunt colorate
-    while any(color == -1 for color in node_color):
-        # Selectează o nouă culoare din paleta de culori
-        active_color = len(color_set)
-        color_set.append(active_color)
+    color_set = []
 
-        # Step 2: Găsește nodul necolorat cu grad maxim
-        highest_degree_node = get_uncolored_highest_degree(degrees, node_color)
-
-        # Step 3: Colorează nodul selectat și actualizează V'
-        while highest_degree_node is not None:
-            node_color[highest_degree_node] = active_color
-            V_prime = update_V_prime(graph, highest_degree_node, node_color)
-
-            # Selectează următorul nod necolorat din V' pentru aceeași culoare
-            next_node = None
-            max_degree = -1
-            for v in V_prime:
-                current_degree = len(graph[v])
-                if current_degree > max_degree:
-                    max_degree = current_degree
-                    next_node = v
-
-            highest_degree_node = next_node
+    # Repetă până toate nodurile sunt colorate
+    for node in degrees:
+        if node_color[node] == -1:  # Nod necolorat
+            current_color = len(color_set)  # Atribuie o nouă culoare
+            color_set.append(current_color)
+            
+            # Colorează nodurile ce pot fi colorate cu aceeași culoare
+            for v in degrees:
+                if node_color[v] == -1 and all(node_color[neighbor] != current_color for neighbor in graph[v]):
+                    node_color[v] = current_color
 
     return len(color_set), node_color
