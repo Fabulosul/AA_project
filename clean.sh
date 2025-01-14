1b80ff6c636e8c8d9b8338ed8b669ff13ee80b36
Mircea <bezneamirceaandrei21@gmail.com>
2025-01-14 22:29:14 +0200

    Done

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to delete all __pycache__ directories in the current directory and subdirectories
-
-echo "Searching for __pycache__ directories..."
-
-find . -type d -name "__pycache__" -exec rm -rf {} +
-
-echo "All __pycache__ directories have been deleted."
